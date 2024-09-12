cb1f5e099a599e6260e965c9721f0988495771ad
coldtimeline <anakingallagher@gmail.com>
2024-09-12 15:13:27 +0200

    created a test_support.py file with a firs function

---
+++ tests/test_support.py
@@ -0,0 +1,17 @@
+from modules.generate_functions import digit_or_special_present
+
+
+def test_digit_or_special_present():
+    """
+    This tests that the test_digit_or_special_present function 
+    correctly checks if digit or punctation are present.
+
+    GIVEN: string with or without digit and punctation
+    WHEN: the function is called with a string
+    THEN: the function returns true only if digit or punctation is present
+    """
+    assert digit_or_special_present("ciao") == False #alphabet string
+    assert digit_or_special_present("ciao1") == True #string with digits
+    assert digit_or_special_present("ciao!") == True #string with punctation
+    assert digit_or_special_present("") == False #empty string
+    assert digit_or_special_present(" ") == False #string with spaces
