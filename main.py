9d30d2f0e5dcd42623c9a7bf5ec364f8913e4ff4
coldtimeline <anakingallagher@gmail.com>
2024-09-16 10:32:02 +0200

    added a main file, with the main function

---
+++ main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from datetime import datetime
+from src.ask_functions import get_name, get_surname, get_gender, get_date_of_birth, get_dataframe_from_web
+from src.ask_functions import get_place_of_birth
+from src.generate_functions import generate_fiscal_code
+
+def run_program():
+    try:
+      name = get_name()
+      surname = get_surname()
+      gender = get_gender()
+      date_of_birth = get_date_of_birth()
+
+      place_dataset = get_dataframe_from_web('https://dait.interno.gov.it/territorio-e-autonomie-locali/sut/elenco_codici_comuni.php')
+      place = get_place_of_birth(place_dataset)
+
+      print("your fiscal code is:")
+      print(generate_fiscal_code(name,surname,gender,date_of_birth,place,place_dataset))
+    except Exception as e:
+      print(e)
+
+run_program()
