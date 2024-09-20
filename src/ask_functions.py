3b192c71b870c24080095b28edfe8ac137eecb07
coldtimeline <anakingallagher@gmail.com>
2024-09-20 16:57:14 +0200

    modified ask_functions to implement read from file
    modified ask_function and their documentation to
    allow running the program with data from a file
    whose extension is ini

--- src/ask_functions.py
+++ src/ask_functions.py
@@ -41,53 +41,82 @@ def get_dataframe_from_html(path_to_page):
 
 
 
-def get_name():
+def get_name(from_ini=False,name=""):
     """
-    Asks the user for their name and checks if it is valid using
-    the is_name_ok function.
+    This function:
+    - if parameter from_ini is True, checks if the parameter name is a valid name
+    - if parameter from_ini is False, ask the user its name and checks if it is valid
+
+    Parameters:
+    from_ini (bool, Optional): represent if the function is called with data from a config file (True) or not (False)
+    name (string, Optional): the name that has been read from a config file
 
     Returns:
-    str: The valid name entered by the user.
+    str: The valid name entered from config file or by the user.
 
     Raises:
     ValueError if the name is not valid.
     """
-    name = input("Please enter your name (ascii format), no digit or punctation allowed: ")
-    if not is_name_ok(name):
-        raise ValueError("The name entered is not valid. No digit or punctation allowed")
+
+    if from_ini:
+        if not is_name_ok(name):
+            raise ValueError("The name entered is not valid. No digit or punctation allowed")
+    else:
+        name = input("Please enter your name (ascii format), no digit or punctation allowed: ")
+        if not is_name_ok(name):
+            raise ValueError("The name entered is not valid. No digit or punctation allowed")
+    
     return name
 
 
-def get_surname():
+def get_surname(from_ini=False, surname=""):
     """
-    Asks the user for their surname and checks if it is valid using
-    the is_surname_ok function.
+    This function:
+    - if parameter from_ini is True, checks if the parameter surname is a valid surname
+    - if parameter from_ini is False, ask the user its surname and checks if it is valid
+
+    Parameters:
+    from_ini (bool, Optional): represent if the function is called with data from a config file (True) or not (False)
+    surname (string, Optional): the surname that has been read from a config file
 
     Returns:
-    str: The valid surname entered by the user.
+    str: The valid surname entered from config file or by the user.
 
     Raises:
     ValueError if the surname is not valid.
     """
-    surname = input("Please enter your surname (ASCII format), no digit or punctation allowed: ")
-    if not is_surname_ok(surname):
-        raise ValueError("The surname entered is not valid. No digit or punctation allowed")
+    if from_ini:
+        if not is_surname_ok(surname):
+            raise ValueError("The surname entered is not valid. No digit or punctation allowed")
+    else:
+        surname = input("Please enter your surname (ASCII format), no digit or punctation allowed: ")
+        if not is_surname_ok(surname):
+            raise ValueError("The surname entered is not valid. No digit or punctation allowed")
     return surname
 
-def get_gender():
+def get_gender(from_ini=False, gender=""):
     """
-    Asks the user for their gender and checks if it is valid using the is_gender_ok function.
-    Raises a ValueError if the gender is not valid.
+    This function:
+    - if parameter from_ini is True, checks if the parameter gender is a valid gender
+    - if parameter from_ini is False, ask the user its gender and checks if it is valid
+
+    Parameters:
+    from_ini (bool, Optional): represent if the function is called with data from a config file (True) or not (False)
+    gender (string, Optional): the gender that has been read from a config file
 
     Returns:
-    str: The valid gender entered by the user.
+    str: The valid gender entered from config file or by the user.
 
     Raises:
     ValueError if the gender is not valid.
     """
-    gender = input("Please enter your gender (M/F): ")
-    if not is_gender_ok(gender):
-      raise ValueError("The gender entered is not valid. Please enter only one m or f")
+    if from_ini:
+        if not is_gender_ok(gender):
+            raise ValueError("The gender entered is not valid. Please enter only one m or f")
+    else:
+        gender = input("Please enter your gender (M/F): ")
+        if not is_gender_ok(gender):
+            raise ValueError("The gender entered is not valid. Please enter only one m or f")
 
     return gender
 
@@ -95,37 +124,51 @@ def get_gender():
 
 
 
-def get_date_of_birth():
+def get_date_of_birth(from_ini=False, date_of_birth_str=""):
     """
-    This function prompts the user for their date of birth.
-    The date of birth should be entered in the format: DD-MM-YYYY.
-
+    This function:
+    - if parameter from_ini is True, transform the parameter date_of_birth_str in a datetime object
+    - if parameter from_ini is False, ask the user its date of birth and transforms
+      it in a datetime object
+    
+    Parameters:
+    from_ini (bool, Optional): represent if the function is called with data from a config file (True) or not (False)
+    date_of_birth_str (string, Optional): represent the date of birth that has been read from config file
+    
     Returns:
     datetime: A datetime object representing the user's date of birth.
 
     Raises:
-    ValueError: datetime.striptime function raises error if the date is not in the
+    ValueError: datetime.strptime function raises error if the date is not in the
     correct format
     """
-    # Prompt the user for their date of birth
-    date_of_birth_str = input("Please enter your date of birth (DD-MM-YYYY): ")
 
-    # Convert the string to a datetime object
-    # If the date is not in the correct format, this function will raise a ValueError
-    date_of_birth = datetime.strptime(date_of_birth_str, "%d-%m-%Y")
+    if from_ini:
+        # If the date is not in the correct format, this function will raise a ValueError
+        date_of_birth = datetime.strptime(date_of_birth_str, "%d-%m-%Y")
+    else:  
+        # Prompt the user for their date of birth
+        # and Convert the string to a datetime object
+        # If the date is not in the correct format, this function will raise a ValueError
+        date_of_birth_str = input("Please enter your date of birth (DD-MM-YYYY): ")
+        date_of_birth = datetime.strptime(date_of_birth_str, "%d-%m-%Y")
 
     return date_of_birth
 
 
 
-def get_place_of_birth(df):
+def get_place_of_birth(df, from_ini=False, place_of_birth=""):
     """
-    This function prompts the user for their place of birth.
+    This function:
+    - if parameter from_ini is True, checks if place_of_birth is valid
+    - if parameter from_ini is False, ask the user its place of birth and checks if is valid
     Then searches for the place in the dataset and returns the right name of the
     place
 
     Parameters:
     df (DataFrame): The dataframe containing the place codes
+    from_ini (bool, Optional): represent if the function is called with data from a config file (True) or not (False) 
+    place_of_birth (string, Optional): the place of birth that has been read from config file
 
     Returns:
     str: The name of the place of birth.
@@ -133,9 +176,13 @@ def get_place_of_birth(df):
     Raises: 
     ValueError if the place of birth contains digit or is empty
     """
-    place_of_birth = input("Please enter your city (comune) of birth: ")
-    if not is_place_of_birth_ok(place_of_birth):
-        raise ValueError("The place of birth entered is not valid. No digit or empty string allowed")
+    if from_ini:
+        if not is_place_of_birth_ok(place_of_birth):
+            raise ValueError("The place of birth entered is not valid. No digit or empty string allowed")
+    else:
+        place_of_birth = input("Please enter your city (comune) of birth: ")
+        if not is_place_of_birth_ok(place_of_birth):
+            raise ValueError("The place of birth entered is not valid. No digit or empty string allowed")
 
     #string to capital letter because string are in capital letter in dataframe we used
     place_of_birth = place_of_birth.upper()
