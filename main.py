6a8ff90b126aaf271fa7a9e5e86c245e4b476af9
coldtimeline <anakingallagher@gmail.com>
2024-09-20 16:58:50 +0200

    modified main file to allow reading data from file
    modified the main file to allow reading data from
    a ini file: in particular the program runs two different function
    according to the presence of a argument given when
    the program is called

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import argparse
+import configparser
+
 import pandas as pd
 from datetime import datetime
 from src.ask_functions import get_name, get_surname, get_gender, get_date_of_birth, get_dataframe_from_html
 from src.ask_functions import get_place_of_birth
 from src.generate_functions import generate_fiscal_code
 
-def run_program():
+def run_program_from_cl():
     try:
       name = get_name()
       surname = get_surname()
@@ -18,4 +21,42 @@ def run_program():
     except Exception as e:
       print(e)
 
-run_program()
+def run_program_from_ini_file(file_path): 
+    try:
+      config = configparser.ConfigParser()
+      config.read(file_path)
+
+      name = get_name(True,config.get('PERSON', 'name'))
+      surname = get_surname(True,config.get('PERSON', 'surname'))
+      gender = get_gender(True,config.get('PERSON', 'gender'))
+      date_of_birth = get_date_of_birth(True,config.get('PERSON', 'date_of_birth'))      
+      place_dataset = get_dataframe_from_html('codici_comuni.htm')
+      place = get_place_of_birth(place_dataset,True,config.get('PERSON', 'place_of_birth'))
+
+      print("your fiscal code is:")
+      print(generate_fiscal_code(name,surname,gender,date_of_birth,place,place_dataset))
+    except Exception as e:
+      print(e)
+   
+   
+
+def main():
+    
+    parser = argparse.ArgumentParser(description="Read user data from config or input")
+    parser.add_argument('--config', type=str, help='Path to the config file')
+    args = parser.parse_args()
+
+    
+    if args.config:
+        run_program_from_ini_file(args.config)
+    else:
+        run_program_from_cl()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
